2df1c8efe8648d44bf690705b7f1aad7d066f48b
Dhiraj <dkskp2005@gmail.com>
2024-05-15 22:16:51 +0530

    client login form

---
+++ client/src/Components/AuthForm/ClientLoginForm.jsx
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Navbar from "../../Components/Navbar/Navbar";
+
+const ClientLoginForm = () => {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      //   console.log("Form data", formData);
+      const response = await fetch(`/clients/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (!response.ok) {
+        throw new Error("Registration failed");
+      }
+
+      const data = await response.json();
+      console.log("Login successful", data);
+
+      // saving worker's token and role in localstorage
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("role", "client");
+      localStorage.setItem("id", data.userId);
+
+      navigate("/");
+      window.location.reload();
+    } catch (error) {
+      console.error("Error registering worker:", error.message);
+      // Handle error, e.g., show an error message to the user
+    }
+  };
+  return (
+    <div style={{ marginTop: "100px" }}>
+      <Navbar />
+
+      <div className="container formContainer section" id="clientLoginForm">
+        <form onSubmit={handleSubmit} className="authForm">
+          <h2>Login as a Client</h2>
+          <div>
+            <input
+              type="email"
+              placeholder="Email"
+              className="infoInput "
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+            <br />
+            <input
+              type="password"
+              placeholder="Password"
+              className="infoInput"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <Link
+              to="/auth/clientRegister"
+              style={{
+                fontSize: "12px",
+                cursor: "pointer",
+              }}
+            >
+              Don't have an account ? Register
+            </Link>
+          </div>
+          <button className="button infoButton" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ClientLoginForm;
